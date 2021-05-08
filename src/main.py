473404b52ca59618a3d638186226798bb5cd78ed
BenOsborn <bengrantalbertosborn@gmail.com>
2021-05-09 00:45:52 +1000

    Update

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 import numpy as np
 from api import API
-import matplotlib.pyplot as plt
 
 # Now I also need some sort of threading function which can run through them synonomously and get the best ones?
 
@@ -9,19 +8,19 @@ def moving_average(array, window):
 
     return mv_avg
 
+# Maybe it is just a better idea to rank by percent change over 10 minutes or something ??????????
 def preprocess(price_data):
-    WINDOW = 20
+    WINDOW = 30 # Play with this
 
-    price_data_spliced = price_data[-60:]
+    price_data_spliced = price_data[-60:] # For the purpose of visualizing the data
+
+    # Maybe instead do a moving average of the slopes?
 
     minute_percent_change = 100 * (price_data_spliced[1:] - price_data_spliced[:-1]) / price_data_spliced[:-1]
     mv_avg = moving_average(price_data_spliced, WINDOW)
 
     percent_price_change = 100 * (mv_avg[1:] - mv_avg[:-1]) / mv_avg[:-1] # Derivative
     percent_slope_change = 100 * (percent_price_change[1:] - percent_price_change[:-1]) / percent_price_change[:-1] # Concavity
-
-    plt.plot(mv_avg)
-    plt.show()
 
     # How can I make some sort of score from these?
     return minute_percent_change[-1], percent_price_change[-1], percent_slope_change[-1]
