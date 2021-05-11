15c1f9429797edaedca08d63535544da8892c5d1
BenOsborn <bengrantalbertosborn@gmail.com>
2021-05-11 23:39:49 +1000

    Update

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from time import sleep
 from tabulate import tabulate
 
 def main(): # Maybe some sort of command line argument here
-    NUM_SYMBOLS = 4000
+    NUM_SYMBOLS = 4000 
     NUM_SYMBOLS_DISPLAY = 7
 
     monitor = Monitor(NUM_SYMBOLS)
