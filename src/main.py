52a7267a355490df686b117fbec5cd60764cd226
BenOsborn <bengrantalbertosborn@gmail.com>
2021-05-11 16:20:49 +1000

    Update

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 from monitor import Monitor
 from time import sleep
-import tabulate
+from tabulate import tabulate
 
 def main(): # Maybe some sort of command line argument here
-    NUM_SYMBOLS = 100
+    NUM_SYMBOLS = 1000
     NUM_SYMBOLS_DISPLAY = 7
 
     monitor = Monitor(NUM_SYMBOLS)
@@ -13,7 +13,11 @@ def main(): # Maybe some sort of command line argument here
         try:
             raw_data = monitor.get_data(NUM_SYMBOLS_DISPLAY)
             table_data = [[data['token_info']['name'], data['token_info']['symbol'], *data['price_data'][:6]] for data in raw_data.values()]
+
             table = tabulate(table_data, headers=["Name", "Symbol", "2hr change", "6hr change", "12hr change", "24hr change", "48hr change", "Recent price"])
+            print(table)
+            print()
+
             sleep(60)
         
         except KeyboardInterrupt:
