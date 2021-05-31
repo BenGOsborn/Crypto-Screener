255a6d601839de5e861245ee94cdd883215467e5
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-05-31 22:54:30 +1000

    Update

--- src/backend/screener/tokens_monitor.py
+++ src/backend/screener/tokens_monitor.py
@@ -37,6 +37,7 @@ class TokensMonitor:
         # Update the token data while this thread runs
         while True:
             # Get the token info and ids
+            print(f"{header}Updating list of tokens")
             token_info = api.get_token_info(num_tokens)
             valid_token_ids = [info['id'] for info in token_info]
 
