b7ccaa4a5eeaa14993e54ef3e6b8164942aa9acc
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-06-17 13:12:25 +1000

    Update

--- src/frontend/components/layout.js
+++ src/frontend/components/layout.js
@@ -9,6 +9,7 @@ export default function Layout(props) {
                 <meta name="description" content="Ranks crypto tokens based on their price changes, volume, and volume abnormality to help you find the ones that are performing the best!" />
                 <meta name="keywords" content="crypto, screener, cryptoscreener, tokens, finance, cryptocurrency" />
                 <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+                <link rel="manifest" href="/manifest.json" />
             </Head>
 
             <Header />
